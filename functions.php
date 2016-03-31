55ea9e6e19fdf478eacad0efca6f6f46668750ab
grange85 <andy@grange85.co.uk>
2016-03-31 14:33:22 +0100

    update author roles

--- functions.php
+++ functions.php
@@ -18,4 +18,22 @@ function tnatheme_globals() {
 }
 tnatheme_globals();
 
+
+function set_theme_capabilities() {
+	$role = get_role( 'author' );
+
+	/* remove "post" capabilities as posts aren't used in this section */
+	$role->remove_cap( 'edit_posts' );
+	$role->remove_cap( 'publish_posts' );
+	$role->remove_cap( 'delete_posts' );
+	$role->remove_cap( 'delete_published_posts' );
+	$role->remove_cap( 'edit_published_posts' );
+
+	/* add page capabilities */
+	$role->add_cap( 'edit_pages' );
+	$role->add_cap( 'edit_others_pages' );
+	$role->add_cap( 'edit_published_pages' );
+}
+add_action ( 'admin_init', 'set_theme_capabilities' );
+
 ?>
